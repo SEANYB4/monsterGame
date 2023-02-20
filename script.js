0aa41e037c01f1d55e7e18a0b1b59021023e9f85
SEANYB4 <bainsean@outlook.com>
2023-02-20 21:05:02 +0800

    Added obstacle class.

--- script.js
+++ script.js
@@ -18,6 +18,9 @@ window.addEventListener('load', () => {
             this.collisionRadius = 50;
             this.speedX = 0;
             this.speedY = 0;
+            this.dx = 0;
+            this.dy = 0;
+            this.speedModifier = 5;
 
         }
 
@@ -49,9 +52,61 @@ window.addEventListener('load', () => {
 
         update() {
 
+            // Technique 1
 
-            this.collisionX = this.game.mouse.x;
-            this.collisionY = this.game.mouse.y;
+            // this.dx = this.game.mouse.x - this.collisionX;
+            // this.dy = this.game.mouse.y - this.collisionY; 
+            // this.speedX = (this.dx)*0.05;
+            // this.speedY = (this.dy)*0.05;
+            // this.collisionX += this.speedX;
+            // this.collisionY += this.speedY;
+
+
+            // Technique 2
+
+            this.dx = this.game.mouse.x - this.collisionX;
+            this.dy = this.game.mouse.y - this.collisionY; 
+            const distance = Math.hypot(this.dy, this.dx);
+            if (distance > this.speedModifier){
+                this.speedX = this.dx/distance || 0;
+                this.speedY = this.dy/distance || 0;
+            } else {
+
+                this.speedX = 0;
+                this.speedY = 0;
+            }
+            
+            this.collisionX += this.speedX * this.speedModifier;
+            this.collisionY += this.speedY * this.speedModifier;
+
+            
+        }
+    }
+
+
+    class Obstacle {
+
+
+        constructor(game){
+
+            this.game = game;
+            this.collisionX = Math.random() * this.game.width;
+            this.collisionY = Math.random() * this.game.height;
+            this.collisionRadius = 100;
+
+        }
+
+        draw(context) {
+            context.beginPath();
+            context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+            context.save();
+            // save method creates a snapshot of the current canvas state, including fillStyle
+            // lineWidth, opacity (globalAlpha) as well as transformations and scaling
+            context.globalAlpha = 0.5;
+            context.fill();
+            // restore method resets canvas settings to what they were before calling save
+            context.restore();
+            context.stroke();
         }
     }
 
@@ -63,6 +118,8 @@ window.addEventListener('load', () => {
             this.width = this.canvas.width;
             this.height = this.canvas.height;
             this.player = new Player(this); // this keyword refers to the entire object
+            this.numberOfObstacles = 5;
+            this.obstacles = [];
             this.mouse = {
                 x: this.width * 0.5,
                 y: this.height * 0.5,
@@ -104,8 +161,11 @@ window.addEventListener('load', () => {
                 // automatically inherit the reference to this keyword from the parent scope
 
                 // offset.x/y coordinates give coordinates on the target node, e.g. canvas
-                this.mouse.x = e.offsetX;
-                this.mouse.y = e.offsetY;
+                if (this.mouse.pressed){
+                    this.mouse.x = e.offsetX;
+                    this.mouse.y = e.offsetY;
+                }
+               
                 
 
             });
@@ -116,16 +176,27 @@ window.addEventListener('load', () => {
         render(context) {
             this.player.draw(context);
             this.player.update();
+            this.obstacles.forEach((obstacle) => {
+                obstacle.draw(context);
+            })
     
         }
+
+        init() {
+            for (let i = 0; i < this.numberOfObstacles; i++){
+                this.obstacles.push(new Obstacle(this));
+            }
+        }
+
+        
 
     }
 
 
 
     const game = new Game(canvas);
-    
-    
+    game.init();
+   
 
 
 
@@ -136,7 +207,6 @@ window.addEventListener('load', () => {
         game.render(ctx);
         // creates an endless animation loop
         requestAnimationFrame(animate);
-
     }
 
 

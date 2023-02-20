04f73d35dd2bb068cefdd9779fbac99c9e8789de
SEANYB4 <bainsean@outlook.com>
2023-02-20 20:05:27 +0800

    Added player movement.

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ window.addEventListener('load', () => {
             this.game = game; // Passed by reference, objects in JS are reference types
             this.collisionX = this.game.width * 0.5;
             this.collisionY = this.game.height * 0.5;
-            this.collisionRadius = 30;
+            this.collisionRadius = 50;
+            this.speedX = 0;
+            this.speedY = 0;
 
         }
 
@@ -33,6 +35,23 @@ window.addEventListener('load', () => {
             context.restore();
             context.stroke();
 
+            // save and restore methods allow us to apply specific drawing settings only to
+            // selected shapes, without affecting the rest of our canvas drawings.
+
+            context.beginPath();
+            context.moveTo(this.collisionX, this.collisionY);
+            context.lineTo(this.game.mouse.x, this.game.mouse.y);
+            // call stroke to actually draw the line
+            context.stroke();
+
+
+        }
+
+        update() {
+
+
+            this.collisionX = this.game.mouse.x;
+            this.collisionY = this.game.mouse.y;
         }
     }
 
@@ -44,12 +63,59 @@ window.addEventListener('load', () => {
             this.width = this.canvas.width;
             this.height = this.canvas.height;
             this.player = new Player(this); // this keyword refers to the entire object
+            this.mouse = {
+                x: this.width * 0.5,
+                y: this.height * 0.5,
+                pressed: false
+            }
+
+            // event listeners - make sure they are automatically applied when creating an instance of the game
+
+            // callback functions on event listeners auto generates an event object that contains
+            // all kinds of information about the event that just happened
+            // to get access to it we just need to give it a name.
+            canvas.addEventListener('mousedown', (e) => {
+
+                // one of the special features of ES6 arrow functions is that they
+                // automatically inherit the reference to this keyword from the parent scope
+
+                // offset.x/y coordinates give coordinates on the target node, e.g. canvas
+                this.mouse.x = e.offsetX;
+                this.mouse.y = e.offsetY;
+                this.mouse.pressed = true;
+
+            });
+
+            canvas.addEventListener('mouseup', (e) => {
+
+                // one of the special features of ES6 arrow functions is that they
+                // automatically inherit the reference to this keyword from the parent scope
+
+                // offset.x/y coordinates give coordinates on the target node, e.g. canvas
+                this.mouse.x = e.offsetX;
+                this.mouse.y = e.offsetY;
+                this.mouse.pressed = false;
+
+            });
+
+            canvas.addEventListener('mousemove', (e) => {
+
+                // one of the special features of ES6 arrow functions is that they
+                // automatically inherit the reference to this keyword from the parent scope
+
+                // offset.x/y coordinates give coordinates on the target node, e.g. canvas
+                this.mouse.x = e.offsetX;
+                this.mouse.y = e.offsetY;
+                
+
+            });
 
         }
 
 
         render(context) {
             this.player.draw(context);
+            this.player.update();
     
         }
 
@@ -58,19 +124,24 @@ window.addEventListener('load', () => {
 
 
     const game = new Game(canvas);
-    game.render(ctx);
-    console.log(game);
+    
+    
 
 
 
 
     function animate() {
-
-
-
+        // clears the old paint
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.render(ctx);
+        // creates an endless animation loop
+        requestAnimationFrame(animate);
 
     }
 
 
+    animate();
 
-})
+
+
+});
